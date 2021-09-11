768c580ef848bb17952c2ef7b2e8251c0f72de8e
unknown <sarrazinmaxime@gmail.com>
2021-09-11 17:58:43 +0200

    Test deploy

--- README.md
+++
@@ -1,2 +0,0 @@
-# redux-project
-A showcase of the results of my exploration of React and the Redux template
--- src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = () => {
-	return (
-		<h2 className="notFound">Nothing to see here</h2>
-	)
-}
-
-export default NotFound;
